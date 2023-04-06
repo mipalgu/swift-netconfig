67514ad7dbc44639cd0466b25faa61a656464a41
Rene Hexel <r.hexel@griffith.edu.au>
2023-04-06 14:06:18 +1000

    Factor out statistics gathering into NetConfig class.

--- Sources/NetConfig/NetConfig.swift
+++ Sources/NetConfig/NetConfig.swift
@@ -7,15 +7,17 @@ import Darwin
 #endif
 public class NetConfig {
     /// The underlying netlink message used for communication.
-    var message = NetlinkMessage()
+    @usableFromInline var message = NetlinkMessage()
     /// The netlink socket used for communication.
-    let fd = socket(AF_NETLINK, CInt(SOCK_RAW.rawValue), CInt(NETLINK_ROUTE))
+    @usableFromInline let fd = socket(AF_NETLINK, CInt(SOCK_RAW.rawValue), CInt(NETLINK_ROUTE))
     /// The name of the interface.
-    let interfaceName: String
+    public let interfaceName: String
     /// The index of the interface
-    let interfaceIndex: UInt32?
+    @usableFromInline let interfaceIndex: UInt32?
+    /// The interface statistics callback.
+    var onStatistics: ((String, LinkStatistics) -> Void)?
     /// Return whether the connection is open.
-    var isOpen = false
+    public var isOpen = false
 
     /// Designated initialiser.
     ///
@@ -28,7 +30,11 @@ public class NetConfig {
 
     deinit {
         guard fd != -1 else { return }
-        close(fd)
+#if os(Linux)
+        Glibc.close(fd)
+#elseif os(macOS)
+        Darwin.close(fd)
+#endif
     }
 }
 
@@ -50,5 +56,35 @@ public extension NetConfig {
 #elseif os(macOS)
         Darwin.close(fd)
 #endif
+    }
+
+    /// Request interface statistics
+    /// - Returns: `true` if the request was successful, `false` otherwise.
+    @inlinable
+    func requestStatistics() -> Bool {
+        guard fd != -1 else { return false }
+        return send_netlink_msg(fd, RTM_GETLINK, NLM_F_REQUEST | NLM_F_DUMP) > 0
+    }
+
+    /// Call the given callback for any interface statistics received.
+    func forEachStatistics(_ callback: @escaping (String, LinkStatistics) -> Void) -> Bool {
+        fd != -1 && withUnsafeMutablePointer(to: &message.rawValue) { msgptr in
+            let messageLength = recv_netlink_msg(fd, msgptr)
+            guard messageLength >= 0 else { return false }
+
+            self.onStatistics = callback
+            let rawSelf = Unmanaged.passUnretained(self).toOpaque()
+            return parse_netlink_msg(msgptr, messageLength, rawSelf) { ifi, stats, ifptr in
+                guard let this = ifptr.map({ Unmanaged<NetConfig>.fromOpaque($0).takeUnretainedValue() }) else { return }
+                let index = this.interfaceIndex
+                guard let ifi, index == nil || ifi.pointee.ifi_index == Int(index!),
+                      let statistics = stats.map(LinkStatistics.init) else { return }
+                var buf = [CChar](repeating: 0, count: Int(IFNAMSIZ))
+                this.onStatistics?(
+                    String(cString: if_indextoname(UInt32(ifi.pointee.ifi_index), &buf)),
+                    statistics
+                )
+            }
+        }
     }
 }
--- Sources/linkstats/main.swift
+++ Sources/linkstats/main.swift
@@ -8,62 +8,23 @@ import Darwin
 #endif
 
 let arguments = CommandLine.arguments
-
-guard arguments.count == 2 else {
-    print("Usage: \(arguments[0]) <interface_name>")
-    exit(1)
-}
-
-let interfaceName = arguments[1]
-let ifIndex = if_nametoindex(interfaceName)
-
-guard ifIndex != 0 else {
-    print("Error: Invalid interface name")
-    exit(1)
-}
-
-let fd = socket(AF_NETLINK, CInt(SOCK_RAW.rawValue), CInt(NETLINK_ROUTE))
-
-guard fd >= 0 else {
+let netconfig = NetConfig(interface: arguments.count == 2 ? arguments[1] : "")
+guard netconfig.open() else {
     perror("Failed to create netlink socket")
-    exit(1)
+    exit(EXIT_FAILURE)
 }
 
-var message = NetlinkMessage()
-guard bind_netlink_msg(fd, &message.rawValue) else {
-    perror("Failed to bind netlink socket")
-    close(fd)
-    exit(1)
-}
-
-let sendResult = send_netlink_msg(fd, RTM_GETLINK, NLM_F_REQUEST | NLM_F_DUMP)
-guard sendResult >= 0 else {
+guard netconfig.requestStatistics() else {
     perror("Failed to send netlink message")
-    close(fd)
-    exit(1)
+    netconfig.close()
+    exit(EXIT_FAILURE)
 }
 
-withUnsafeMutablePointer(to: &message.rawValue) { msgptr in
-    let messageLength = recv_netlink_msg(fd, msgptr)
-    guard messageLength >= 0 else {
-        perror("Failed to receive netlink message")
-        close(fd)
-        exit(1)
-    }
-
-//    print("Parsing message \(String(describing: msgptr)) of length: \(messageLength)")
-    guard parse_netlink_msg(msgptr, messageLength, UnsafeMutableRawPointer(bitPattern: Int(ifIndex)), { ifi, stats, ifptr in
-        let ifIndex = Int(bitPattern: ifptr)
-        guard let ifi, ifi.pointee.ifi_index == ifIndex,
-              let statistics = stats.map(LinkStatistics.init) else { return }
-        var buf = [CChar](repeating: 0, count: Int(IFNAMSIZ))
-        print("Interface: \(String(cString: if_indextoname(UInt32(ifi.pointee.ifi_index), &buf)))")
-//        printInterfaceStats(stats: stats)
-        print(statistics.description)
-    }) else {
-        perror("Failed to parse netlink message")
-        close(fd)
-        exit(1)
-    }
+guard netconfig.forEachStatistics({
+    print("Interface: \($0)")
+    print($1.description)
+}) else {
+    perror("Failed to receive netlink message")
+    netconfig.close()
+    exit(EXIT_FAILURE)
 }
-close(fd)
